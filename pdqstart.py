d7b35dbe7c3dc1b72b0a646239a5f962cb84d5d0
Dan Rahmel <drahmel7@gmail.com>
2011-05-30 09:02:40 -0700

    DJR:Restructured for static content

--- pdqstart.py
+++ pdqstart.py
@@ -1,7 +1,37 @@
-import tornado.ioloop
-import tornado.web
+#!/usr/bin/env python
 
-class MainHandler(tornado.web.RequestHandler):
+import os.path
+import re
+import tornado.auth
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+import unicodedata
+
+from tornado.options import define, options
+
+define("port", default=7000, help="run on the given port", type=int)
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r"/", HomeHandler)
+        ]
+        settings = dict(
+            blog_title=u"PDQueue Admin",
+            template_path=os.path.join(os.path.dirname(__file__), "templates"),
+            static_path=os.path.join(os.path.dirname(__file__), "static"),
+            cookie_secret="11oETzKXQAGaYdkL5gEmGeJJFuYh7EQnp2XdTP1o/Vo="
+        )
+        tornado.web.Application.__init__(self, handlers, **settings)
+
+class BaseHandler(tornado.web.RequestHandler):
+    @property
+    def db(self):
+        pass
+
+class HomeHandler(BaseHandler):
     def get(self):
     	f = open('index.html','r')
     	page = f.read()
@@ -10,9 +40,16 @@ class MainHandler(tornado.web.RequestHandler):
         self.write(page)
 
 application = tornado.web.Application([
-    (r"/", MainHandler),
+    (r"/", HomeHandler),
 ])
 
-if __name__ == "__main__":
-    application.listen(7000)
+def main():
+    tornado.options.parse_command_line()
+    http_server = tornado.httpserver.HTTPServer(Application())
+    http_server.listen(options.port)
     tornado.ioloop.IOLoop.instance().start()
+
+
+if __name__ == "__main__":
+    main()
+
