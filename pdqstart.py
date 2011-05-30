8f28f91a4464d3719c10a0ae72d14ea7550ca167
Dan Rahmel <drahmel7@gmail.com>
2011-05-30 14:45:56 -0700

    DJR:Initial post and added favicon

--- pdqstart.py
+++ pdqstart.py
@@ -14,42 +14,50 @@ from tornado.options import define, options
 define("port", default=7000, help="run on the given port", type=int)
 
 class Application(tornado.web.Application):
-    def __init__(self):
-        handlers = [
-            (r"/", HomeHandler)
-        ]
-        settings = dict(
-            blog_title=u"PDQueue Admin",
-            template_path=os.path.join(os.path.dirname(__file__), "templates"),
-            static_path=os.path.join(os.path.dirname(__file__), "static"),
-            cookie_secret="11oETzKXQAGaYdkL5gEmGeJJFuYh7EQnp2XdTP1o/Vo="
-        )
-        tornado.web.Application.__init__(self, handlers, **settings)
+	def __init__(self):
+		handlers = [
+			(r"/", HomeHandler),
+			(r"/job/([0-9a-zA-Z_]+)", JobHandler),
+
+		]
+		settings = dict(
+			blog_title=u"PDQueue Admin",
+			template_path=os.path.join(os.path.dirname(__file__), "templates"),
+			static_path=os.path.join(os.path.dirname(__file__), "static"),
+			cookie_secret="11oETzKXQAGaYdkL5gEmGeJJFuYh7EQnp2XdTP1o/Vo="
+		)
+		tornado.web.Application.__init__(self, handlers, **settings)
 
 class BaseHandler(tornado.web.RequestHandler):
-    @property
-    def db(self):
-        pass
+	@property
+	def db(self):
+		pass
 
 class HomeHandler(BaseHandler):
-    def get(self):
-    	f = open('index.html','r')
-    	page = f.read()
-    	f.close()
-    	#"<img src='https://github.com/drahmel/PDQueue/blob/master/pdq-logo.png?raw=true' /><br/>PDQueue Started"
-        self.write(page)
-
-application = tornado.web.Application([
-    (r"/", HomeHandler),
-])
+	def get(self):
+		f = open('index.html','r')
+		page = f.read()
+		f.close()
+		#"<img src='https://github.com/drahmel/PDQueue/blob/master/pdq-logo.png?raw=true' /><br/>PDQueue Started"
+		self.write(page)
+class JobHandler(BaseHandler):
+	def get(self,jobname):
+		self.write('Info on job:'+jobname)
+		self.write('<html><body><form action="/job/'+jobname+'" method="post">'
+				   '<input type="text" name="message">'
+				   '<input type="submit" value="Submit">'
+				   '</form></body></html>')
+	def post(self,jobname):
+		self.set_header("Content-Type", "text/plain")
+		self.write("You posted " + self.get_argument("message")+" to job "+jobname)
 
 def main():
-    tornado.options.parse_command_line()
-    http_server = tornado.httpserver.HTTPServer(Application())
-    http_server.listen(options.port)
-    tornado.ioloop.IOLoop.instance().start()
+	tornado.options.parse_command_line()
+	http_server = tornado.httpserver.HTTPServer(Application())
+	http_server.listen(options.port)
+	tornado.ioloop.IOLoop.instance().start()
 
 
 if __name__ == "__main__":
-    main()
+	main()
 
